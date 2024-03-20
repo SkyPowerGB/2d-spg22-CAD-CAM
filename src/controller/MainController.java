c529e3205abae5e5d5abca0e8499b34d82ed6db3
45bru <pyrobrunogb@gmail.com>
2024-03-20 06:13:07 +0100

    priprema za poboljsani zoom

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -3,9 +3,12 @@ package controller;
 import View.MainView;
 import controller.callbacks.NewFileCallBack;
 import helpers.enums.FileOptionsE;
+import helpers.helperModels.Line;
 import model.FileData;
 
 import javax.swing.*;
+import javax.swing.text.View;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseWheelEvent;
@@ -14,39 +17,30 @@ import java.util.ArrayList;
 
 public class MainController implements NewFileCallBack {
     MainView view;
-    public MainController(){
 
-       view =new MainView();
+    public MainController() {
 
-        ArrayList<JMenuItem>  menuOptions= view.getMenuOptions();
+        view = new MainView();
+
+        ArrayList<JMenuItem> menuOptions = view.getMenuOptions();
 
 
-        for (JMenuItem item:menuOptions) {
+        for (JMenuItem item : menuOptions) {
             item.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                  JMenuItem option = (JMenuItem) e.getSource();
-                  menuOptionClicked(option.getName());
+                    JMenuItem option = (JMenuItem) e.getSource();
+                    menuOptionClicked(option.getName());
                 }
             });
         }
 
-      JPanel workspace=  view.getWorkspacePanel();
+        JPanel workspace = view.getWorkspacePanel();
         workspace.addMouseWheelListener(new MouseWheelListener() {
             @Override
             public void mouseWheelMoved(MouseWheelEvent e) {
-              System.out.println(e.getWheelRotation());
-              double scale=view.getScale();
-              if(e.getWheelRotation()<0){
-                  System.out.println("scroll down");
-              scale=scale-(1*0.01);
-              }else{
-                  scale=scale+(1*0.01);
-                  System.out.println("scroll in");
-              }
 
-              view.setScale(scale);
-            view.scale();
+
             }
         });
 
@@ -54,22 +48,29 @@ public class MainController implements NewFileCallBack {
     }
 
 
-    private void menuOptionClicked(String name){
+    private void menuOptionClicked(String name) {
         System.out.println(name);
 
-        if(name.contentEquals(FileOptionsE.new_file.toString())){
-            NewFileController controllerB= new NewFileController(this);
+        if (name.contentEquals(FileOptionsE.new_file.toString())) {
+            NewFileController controllerB = new NewFileController(this);
         }
     }
 
+
+    public void scale(int factor , Point position){
+
+        Point materialPos= view.getMaterialPos();
+        Line ln=new Line(position,materialPos);
+
+
+    }
 
     @Override
     public void onFileCreate(FileData data) {
 
         view.loadMaterial(data);
-view.refreshWindow();
+        view.refreshWindow();
     }
-
 
 
 
---
+++ src/helpers/helperModels/Line.java
@@ -0,0 +1,38 @@
+package helpers.helperModels;
+
+import java.awt.*;
+
+public class Line {
+    private double k = 0;
+    private double l = 0;
+
+
+    public Line(Point a, Point b) {
+        calcKL(a, b);
+    }
+
+    public Line(int x, int y, int x2, int y2) {
+        calcKL(new Point(x, y), new Point(x2, y2));
+    }
+
+    private void calcKL(Point a, Point b) {
+        if ((b.x - a.x) == 0) {
+            k = 0;
+        } else {
+            k = (double) (b.y - a.y) / (b.x - a.x);
+        }
+
+        if (k == 0) {
+            l = a.y;
+            return;
+        }
+        l = -(k * a.x - a.y);
+
+    }
+
+    public int calcY(int x) {
+        return (int) ((k * x) + l);
+    }
+
+
+}
